9f410e7928b86673a1a8af761fed36118a49f552
sheva77 <andreyshevchenko1177@gmail.com>
2021-02-10 14:30:39 +0200

    -

--- chat_final_bootstrap/src/App.js
+++ chat_final_bootstrap/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    NavLink,
+} from "react-router-dom";
+import createHistory from "history/createBrowserHistory";
+import logo from "./images/logo23.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Logo = () => <img src={logo} width="50px" />;
+
+const Header = () => (
+    <div>
+        Header
+        <Link to={"/"}>
+            <Logo />
+        </Link>
     </div>
-  );
-}
+);
+
+const menuItems = {
+    "/settings": "Настройки",
+    "/newchat": "Новый чат",
+    "/serchChat": "Поиск чата",
+    // "": "",
+};
+
+const MenuItem = ({ url, text }) => (
+    <li>
+        <Link to={url}>{text}</Link>
+    </li>
+);
+
+const Sidebar = () => (
+    <aside>
+        Sidebar
+        <nav>
+            <ul>
+                {Object.entries(menuItems).map(([url, text]) => (
+                    <MenuItem url={url} text={text} />
+                ))}
+            </ul>
+        </nav>
+    </aside>
+);
+
+const ChatContent = () => <aside>ChatContent</aside>;
+
+const PageMain = () => (
+    <div>
+        <Sidebar />
+        <ChatContent />
+    </div>
+);
+
+const PageLogin = () => <div>PageLogin</div>;
+
+const Footer = () => <div>Footer</div>;
+
+const App = () => (
+    <Router history={createHistory()}>
+        <Header />
+        <div>
+            <Route path="/main" component={PageMain} exact />
+            <Route path="/" component={PageLogin} exact />
+        </div>
+        <Footer />
+    </Router>
+);
 
 export default App;

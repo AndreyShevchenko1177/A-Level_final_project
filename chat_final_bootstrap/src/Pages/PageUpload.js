7078175813727b0a70888a365e8a75ec092ca321
sheva77 <andreyshevchenko1177@gmail.com>
2021-03-02 18:37:04 +0200

    multi upload

--- chat_final_bootstrap/src/Pages/PageUpload.js
+++ chat_final_bootstrap/src/Pages/PageUpload.js
@@ -4,36 +4,36 @@ import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 
 export const PageUpload = () => {
-    const formRef = useRef(null);
-    const [uploadId, setUploadId] = useState("");
-    const [uploadUrl, setUploadUrl] = useState("");
     const [fl, setFl] = useState(false);
+    const resultArray = useRef([]);
 
     function MyDropzone() {
-        const onDrop = useCallback((acceptedFiles) => {
+        const onDrop = useCallback(async (acceptedFiles) => {
             // console.log("МАССИВ", typeof acceptedFiles[0], acceptedFiles[0]);
 
-            let data = new FormData();
-            data.set("media", acceptedFiles[0]);
+            setFl(false);
 
-            // console.log(data, typeof data);
-            // for (let [name, value] of data) {
-            //     console.log(name, typeof name, value, typeof value);
-            // }
+            let aaryOfFatchs = acceptedFiles.map((file) => {
+                let dataSingl = new FormData();
+                dataSingl.set("media", file);
+                return fetch(`${urlUploadConst}/upload`, {
+                    method: "POST",
+                    headers: localStorage.authToken ? { Authorization: "Bearer " + localStorage.authToken } : {},
+                    body: dataSingl,
+                })
+                    .then((res) => res.json())
+                    .catch((e) => alert("Произошла ошибка."));
+            });
 
-            fetch(`${urlUploadConst}/upload`, {
-                method: "POST",
-                headers: localStorage.authToken ? { Authorization: "Bearer " + localStorage.authToken } : {},
-                body: new FormData(formRef.current),
-            })
-                .then((res) => res.json())
-                .then((res) => {
-                    console.log(res);
-                    setUploadId(res._id);
-                    setUploadUrl(res.url);
-                });
+            resultArray.current = [];
 
-            //
+            await Promise.all(aaryOfFatchs).then((responses) =>
+                responses.forEach((response) => resultArray.current.push(response))
+            );
+
+            console.log(resultArray.current);
+
+            setFl(true);
         }, []);
 
         const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
@@ -50,41 +50,17 @@ export const PageUpload = () => {
         );
     }
 
-    const doUpload = async () => {
-        setFl(false);
-        let formData = new FormData(formRef.current);
-        // console.log(formData, typeof formData);
-        for (let [name, value] of formData) {
-            console.log(name, typeof name, value, typeof value);
-        }
-
-        // await fetch(`${urlUploadConst}/upload`, {
-        //     method: "POST",
-        //     headers: localStorage.authToken ? { Authorization: "Bearer " + localStorage.authToken } : {},
-        //     body: new FormData(formRef.current),
-        // })
-        //     .then((res) => res.json())
-        //     .then((res) => {
-        //         setUploadId(res._id);
-        //         setUploadUrl(res.url);
-        //     });
-
-        setFl(true);
-    };
-
     return (
         <>
             <div>
-                PageUpload
-                <form action={`${urlUploadConst}/upload`} method="post" encType="multipart/form-data" ref={formRef}>
-                    <input type="file" name="media" onChange={doUpload} />
-                </form>
-                {`_id: ${uploadId}, url: ${uploadUrl}`}
+                PageUpload <br />
                 <br />
-                {fl && <img src={`${urlUploadConst}/${uploadUrl}`} />}
             </div>
             <br />
             <MyDropzone />
+            {resultArray.current.map((res) => (
+                <img src={`${urlUploadConst}/${res.url}`} width="100px" key={res._id} />
+            ))}
         </>
     );
 };

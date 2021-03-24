3b54be7d4ba5c6d7d1c4a6f16dac58fe60748c97
sheva77 <andreyshevchenko1177@gmail.com>
2021-03-24 11:59:12 +0200

    small fix _cancel_

--- chat_final_bootstrap/src/Components/ButtonCansel.js
+++ chat_final_bootstrap/src/Components/ButtonCansel.js
@@ -6,7 +6,7 @@ export const ButtonCancel = ({ cansel = () => history.goBack() }) => (
     <>
         <Button className="gradient rounded-3 ms-2 mb-2" variant="secondary btn-sm" onClick={cansel}>
             <i className="bi bi-x-square"></i>
-            <span className="ms-2">Cansel</span>
+            <span className="ms-2">Cancel</span>
         </Button>
     </>
 );
